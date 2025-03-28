3c07e2d5c22015afd3e4568e3deead665db170c3
Mohammed-Amine <125489574+amilemia@users.noreply.github.com>
2025-03-28 04:28:59 +0000

    Added functionality + responsiveness
    -Fixed the nav bar.
    -Added responsiveness for different sizes (large, tablet, mobile)
    -Added a hamburger nav bar for tablet and mobile navigation
    -Js functionality for tablet/mobile nav

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// Toggle mobile navigation
+const toggleButton = document.getElementById('hamburger-toggle');
+const mobileNav = document.getElementById('mobile-nav');
+toggleButton.addEventListener('click', () => {
+    mobileNav.classList.toggle('active');
+    // Toggle button text for UX
+    if(mobileNav.classList.contains('active')) {
+        toggleButton.textContent = '×';
+        toggleButton.setAttribute('aria-label', 'Close navigation menu');
+    } else {
+        toggleButton.textContent = '☰';
+        toggleButton.setAttribute('aria-label', 'Open navigation menu');
+    }
+});
+// Close mobile nav automatically when a link is clicked
+const mobileLinks = document.querySelectorAll('.mobile-nav nav ul li a');
+mobileLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        mobileNav.classList.remove('active');
+        toggleButton.textContent = '☰';
+        toggleButton.setAttribute('aria-label', 'Open navigation menu');
+    });
+});
